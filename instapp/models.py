9942aa66ef4c28ca970256d3ba9e51951a69b00f
Misigomartin <Misigomartin@gmail.com>
2022-06-07 09:34:05 +0300

    added login form

---
+++ instapp/models.py
@@ -0,0 +1,43 @@
+
+from re import I
+from django.db import models
+
+# Create your models here.
+
+ 
+class Image(models.Model):
+   image = models.ImageField(upload_to='pic/%y/',blank=True)
+   img_name = models.CharField(max_length=200, blank=True)
+   imge_caption = models.CharField(max_length=200,blank=True)
+   date_posted = models.DateTimeField(auto_now_add=True,blank=True)
+   
+   def __str__(self):
+       return self.img_name
+
+class Profile(models.Model):
+   name = models.CharField(max_length=200, blank=True)
+   username = models.CharField(max_length=200, blank=True)
+   profile_photo = models.ImageField(upload_to='pic/%y/',blank=True)
+   image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+   bio = models.TextField(max_length=200,blank=True)
+ 
+   def __str__(self):
+       return self.name
+   
+class Comments(models.Model):
+   comment = models.TextField(max_length=200,null=True, blank=True)
+   image = models.ForeignKey(Image,on_delete=models.CASCADE,null=True,blank=True)
+   author = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True)
+ 
+   def __str__(self):
+       return self.comment
+
+class Likes(models.Model):
+    user = models.ForeignKey(Profile, related_name='likes', on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, related_name='likes', on_delete=models.CASCADE)
+
+class Login(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=20)
+
+# Create your models here.
